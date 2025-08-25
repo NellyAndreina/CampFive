33d60693c8df66c4048d97f85164c709be395e88
Nelly Andreina Mejias Tortolero <nelly.mejias@applydigital.com>
2025-08-25 17:13:16 -0400

    Actualización de Inicio de sesión IMDb

--- src/page_objects/loginPage.js
+++ src/page_objects/loginPage.js
@@ -2,7 +2,6 @@ class LoginPage {
   constructor(page) {
     this.page = page;
     this.signInButton = page.locator('text=Iniciar sesión').first();
-    //this.signInWithImdbLink = page.locator('text=a.list-group-item[href*="signin?openid.pape.max_auth_age=0"]');
     this.signInWithImdbLink = page.getByRole('link', { name: /Inicia sesión con IMDb/i });
     this.emailField = page.locator('#ap_email');
     this.passwordField = page.locator('#ap_password');
@@ -24,7 +23,6 @@ class LoginPage {
   }
 
   async login(email, password) {
-  
     await this.emailField.fill(email);
     await this.passwordField.fill(password);
     await this.submitButton.click();
